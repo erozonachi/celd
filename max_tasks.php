56b3bbd9443d3d9d358c419abb7c7c91b7eadba3
erozonachi <35371532+erozonachi@users.noreply.github.com>
2018-01-13 02:00:01 +0100

    Update max_tasks.php
    PHP code to calculate the maximum number of tasks a military unit can accomplish in limited time, 'X'. Using the greedy algorithm.

--- max_tasks.php
+++ max_tasks.php
@@ -2,17 +2,36 @@
 /** 
 Author - Eneh James
 Description - PHP code snipet to calculate the maximum number of tasks a military unit can accomplish in limited time, 'X'. Using the greedy algorithm.
-Date/Time - 12/01/2018 / 12:12:00
+Date/Time - 12/01/2018 / 21:12:00
 **/
 	define("X", 12);						//Constant 'X' holding limited time value in minutes
 		
 	$A = array(7, 6, 5, 3, 4, 2, 1);		//Integer array 'A' holding the time value it will take to complete different tasks
+
+	$currentTime = 0; //Declaration and Initialization of currentTime variable
+
+	$numberOfThings = 0; //Declaration and Initialization of numberOfThings variable
+
+	sort($A);			//Sort A array in non-decreasing order
+
+
+	for($i=0;$i<count($A);$i++) {					//Iteration block starts here
+
+		$currentTime += $A[$i];	//Add completion time of the current to-do item to currentTime variable
+
+		if($currentTime > X)					//Test if currentTime is greater than X
+
+			break;								//Breaks out of the loop if currentTime is greater than X
+
+		$numberOfThings++;						//Increment numberOfThings variable by one (1)
+		
+	}											//Iteration block stops here
 ?>
 <!DOCTYPE html>
 <html lang="en">
 	<head>
 		<meta charset="utf-8">
-		<title>Military Unit</title>
+		<title>Maximum Tasks</title>
 
 		<style type="text/css">
 
@@ -39,21 +58,6 @@ Date/Time - 12/01/2018 / 12:12:00
 		</style>
 	</head>
 	<body>
-		<?php 
-			$currentTime = 0; //Declaration and Initialization of currentTime variable
-
-			$numberOfThings = 0; //Declaration and Initialization of numberOfThings variable
-
-			sort($A);			//Sorting A array in non-decreasing order
-
-			while ($currentTime <= X) {					//Iteration block starts here
-
-				$currentTime += $A[$numberOfThings];	//Adding completion time of the selected to-do item to currentTime variable
-
-				$numberOfThings++;						//Incrementing numberOfThings variable by one (1)
-				
-			}											//Iteration block stops here
-		 ?>
 			<span>The maximum number of tasks to be completed by the military unit, in limited time of 
 			<?php echo X . " minutes"; ?> is: </span><strong><?php echo $numberOfThings; ?></strong>
 	</body>
